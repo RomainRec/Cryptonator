864228b97bc2c400475ccca823b58f41496a6b12
RomainRec <romain.recher@gmail.com>
2018-11-08 02:57:37 +0100

    scrap ok

---
+++ app/services/scraper.rb
@@ -0,0 +1,39 @@
+require 'nokogiri'
+require 'rubygems'
+require 'open-uri'
+
+class Scraper
+
+#attr_accessor :price, :cryto_name
+
+ def get_price
+	  doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    price = doc.css('a.price').map { |i| i.text } #converti les valeurs en texte
+#   @crypto_price = []
+#	  @crypto_price << price
+end
+
+def get_name
+	  doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    name = doc.css('a.currency-name-container').map { |i| i.text }
+#   @crypto_name = []
+#	  @crypto_name << name
+end
+
+def hash
+    #@hash_crypto = []
+    @hash_crypto = Hash[get_name.zip(get_price)]
+end
+
+def save
+    @hash_crypto.each do |k, v| i = Crypto.create(name: k,price: v)
+    end
+  end
+
+def perform
+    puts hash
+    puts save
+    return @hash_crypto
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'static#home'
+  resources :cryptos
+  get  '/index',    to: 'cryptos#index'
+
+  post '/reset_db', to: 'cryptos#reset_db'
+  post '/scraper', to: 'cryptos#scraper'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
